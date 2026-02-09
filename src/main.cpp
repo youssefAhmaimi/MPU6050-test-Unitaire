35220fd413dc9abc23edd6a3fa7ae40183ce7ddc
youssefAhmaimi <youssefahm.pro@gmail.com>
2026-02-09 15:59:52 +0100

    Batterie+PWM

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,58 @@
+#include <Arduino.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include <math.h>
-#include <String.h>
+#include <BluetoothSerial.h>
 
-Adafruit_MPU6050 mpu;
+unsigned char PWMG = 17;                                    // PWM (vitesse) du moteur gauche
+unsigned char PWMD = 16;                                    // PWM (vitesse) du moteur droit
+char Batterie = 25;                                         // Broche de lecture de la batterie 
 
-float angle,TetaG,TetaW;
+BluetoothSerial SerialBT;                                   // Objet Bluetooth Serial
+Adafruit_MPU6050 mpu;                                       // Objet pour le capteur MPU6050
+
+float angle,TetaG,TetaW;                                    // angle de l'inclinaison du gyropode, angle du gyroscope, angle de l'accélération
 float gyro;
 char FlagCalcul = 0;
 float TetaWF,TetaGF,Ve, Vs = 0;
-float Te = 10;    // période d'échantillonage en ms
-float Tau = 1000; // constante de temps du filtre en ms
-float A, B;// coefficient du filtre
+float Te = 10;                                              // période d'échantillonage en ms
+float Tau = 1000;                                           // constante de temps du filtre en ms
+float A, B;                                                 // coefficient du filtre
+char kp = 5;                                              // coefficient de proportionnalité du correcteur
+
+float R1= 22000.0;                                          // résistance de 22 kohms
+float R2= 10000.0;                                          // résistance de 10 kohms
+float valeurbatterie;
+
+unsigned int frequence = 20000, valPWM = 2048;              // Fréquence de 20 kHz
+unsigned char canal0 = 0;                                   // Canal 0 pour le moteur gauche
+unsigned char canal1 = 1;                                   // Canal 1 pour le moteur droit
+unsigned char resolution = 10;                              // Résolution de 10 bits (valeurs de 0 à 1023)
+
 
 void controle(void *parameters)
 {
-  TickType_t xLastWakeTime;
-  xLastWakeTime = xTaskGetTickCount();
+  TickType_t xLastWakeTime;                                 // Variable pour stocker le temps de réveil de la tâche
+  xLastWakeTime = xTaskGetTickCount();                      // Initialisation du temps de réveil de la tâche
   while (1)
   {
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+    sensors_event_t a, g, temp;                             // Création d'objets pour stocker les données du capteur                        
+    mpu.getEvent(&a, &g, &temp);                            // Lecture des données du capteur
    
-    TetaG=-atan2(a.acceleration.y, a.acceleration.x); // Angle du gyroscope en radian
-    TetaGF = A * TetaG + B * TetaGF;                  // Angle du gyroscope filtré en radian
+    TetaG=-atan2(a.acceleration.y, a.acceleration.x);       // Angle du gyroscope en radian
+    TetaGF = A * TetaG + B * TetaGF;                        // Angle du gyroscope filtré en radian
    
-    TetaW  =  g.gyro.z * Tau/1000;                    // angle de l'accélération en radian  
-    TetaWF = A* TetaW + B*TetaWF;                     // angle de l'accélération filtré en radian
+    TetaW  =  g.gyro.z * Tau/1000;                          // angle de l'accélération en radian  
+    TetaWF = A* TetaW + B*TetaWF;                           // angle de l'accélération filtré en radian
  
-    angle = TetaWF + TetaGF;                          // angle de l'inclinaison du gyropode en radian
+    angle = TetaWF + TetaGF;                                // angle de l'inclinaison du gyropode en radian
 
-    FlagCalcul = 1;
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(Te));
+    FlagCalcul = 1;                                         // Indicateur que les calculs sont terminés
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(Te));     // Attente jusqu'au prochain cycle d'exécution de la tâche
   }
 }
 
-void Vin(void *parameters)
+void Vin(void *parameters)                                  // Tâche pour la lecture de la tension de la batterie
 {
   Ve = 1;
   while (1)
@@ -51,11 +67,21 @@ void Vin(void *parameters)
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.printf("Bonjour \n\r");
+  
+  SerialBT.begin("ESP32test");                              // Bluetooth device name
+  SerialBT.println("Hello from ESP32!");                    // Message de bienvenue pour le Bluetooth
+
+  // Configuration de la PWM
+  ledcSetup(canal0, frequence, resolution);               
+  ledcSetup(canal1, frequence, resolution);                 
  
-  // Try to initialize!
+  // Liaison des canaux PWM aux broches
+  ledcAttachPin(PWMG, canal0);        
+  ledcAttachPin(PWMD, canal1);
+
+  // Initialisation du capteur MPU6050
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -73,12 +99,12 @@ void setup()
       NULL        // descripteur
   );
   xTaskCreate(
-      Vin,   // nom de la fonction
-      "Vin", // nom de la tache que nous venons de vréer
-      10000, // taille de la pile en octet
-      NULL,  // parametre
-      1,     // bas niveau de priorite
-      NULL   // descripteur
+      Vin,        // nom de la fonction
+      "Vin",      // nom de la tache que nous venons de vréer
+      10000,      // taille de la pile en octet
+      NULL,      // parametre
+      1,          // bas niveau de priorite
+      NULL        // descripteur
   );
 
   // calcul coeff filtre
@@ -124,7 +150,11 @@ void reception(char ch)
       A = 1 / (1 + Tau / Te);
       B = Tau / Te * A;
     }
-
+    
+    if (commande == "valPWM")
+    {
+      valPWM = valeur.toInt();
+    }
     chaine = "";
   }
   else
@@ -137,11 +167,17 @@ void loop()
 {
   if (FlagCalcul == 1)
   {
-    Serial.printf("%f %f %f %f\n",TetaG,TetaGF,TetaWF,angle); // Affichage des angles sur le moniteur série
+ //Serial.printf("%f %f %f %f\n",TetaG,TetaGF,TetaWF,angle); // Affichage des angles sur le moniteur série
 
     FlagCalcul = 0;
   }
+  valPWM = kp * angle;                                     // Calcul de la valeur de la PWM en fonction de l'angle d'inclinaison du gyropode
+  ledcWrite(canal0, valPWM);
+  ledcWrite(canal1, valPWM);
+  valeurbatterie=(((3.3/4095.0)*analogRead(Batterie)*(R1+R2))/R2)+0.3;     // Calcul de la valeur de la batterie en volts
+  Serial.printf("valBatterie: %.4f \n", valeurbatterie);            // Affichage de la valeur de la batterie sur le moniteur série
 }
+
 void serialEvent()
 {
   while (Serial.available() > 0) // tant qu'il y a des caractères à lire
@@ -149,3 +185,6 @@ void serialEvent()
     reception(Serial.read());
   }
 }
+
+
+
